c9c46480bb7bda98e97ee4a79d7af4fc0b209aba
Ahri <ahripyx@gmail.com>
2026-04-16 07:37:05 -0400

    Clearing default react design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>This is me testing a pragraph</p>
-      </header>
+    <div>
+      <h1>Weather App</h1>
+
+      <div>
+        <input type='text' placeholder='entery city'/>
+        <button>
+          Search
+        </button>
+      </div>
+      
+      <div>
+        <h2></h2>
+      </div>
+
     </div>
   );
 }
