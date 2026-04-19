b161a1e6fc0ce85c16107b2875fecff5a865f8af
Ahripyx <Ahripyx@LAPTOP-OKERT5CM>
2026-04-19 09:40:39 -0400

    Added more descriptive forecast page layout. Still needs to be designed

--- src/pages/forecast.js
+++ src/pages/forecast.js
@@ -1,11 +1,35 @@
 function Forecast() {
     return (
-        <div>
-            <h2>Forecast</h2>
+        <main>
+            <section>
+                <h2>Forecast</h2>
+                <p>Check your cities weather</p>
+                <p>Search location to view current weather.</p>
+            </section>
 
-            <input type="text" placeholder="enter city" />
-            <button>Search</button>
-        </div>
+            <section>
+                <div>
+                    <input type="text" placeholder="Enter city" />
+                    <button>Search</button>
+                </div>
+            </section>
+
+
+            {/* Values hard coded until search engine works */}
+            <section>
+                <article>
+                    <h3>Current Weather</h3>
+                    <p>City:--</p>
+                    <p>Temperature: --</p>
+                    <p>Condition: --</p>
+                </article>
+
+                <article>
+                    <h3>Upcoming Forecast</h3>
+                    <p>No forecast panel yet until search yabba dabba doo</p>
+                </article>
+            </section>
+        </main>
     );
 }
 
